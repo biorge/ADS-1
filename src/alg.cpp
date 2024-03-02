313b73ff119d2c82077e6eab2ef23c4b624d33a7
Anton <150729033+biorge@users.noreply.github.com>
2024-03-02 17:55:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,79 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (uint64_t i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  bool flag = 0;
+  uint64_t count=0;
+  uint64_t num = 3;
+  uint64_t i = 1;
+  if(n==1){
+    return 2;
+    flag == 1;
+  }
+  if (n == 2) {
+    return 3;
+    flag == 1;
+  }
+  while (flag !=1){
+    count=0;
+    for (uint64_t j = 2; j <= sqrt(num); j++) {
+      if (num % j == 0) {
+        count++;
+      }
+    }
+    if(count==0){
+      i++;
+    }
+    if ((i==n)){
+      flag=true;
+      return(num);
+    }
+    num+=2;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  bool flag = 0;
+  uint64_t count = 0;
+  uint64_t num = value+1;
+  while (flag !=1){
+    count=0;
+    for (uint64_t j = 2; j <= sqrt(num); j++) {
+      if (num % j == 0) {
+        count++;
+      }
+    }
+    if(count==0){
+      return(num);
+      flag = 1;
+    }
+    num++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  uint64_t count=0;
+  uint64_t num = 3;
+  uint64_t sum = 2;
+  while (num<hbound){
+    count=0;
+    for (uint64_t j = 2; j <= sqrt(num); j++) {
+      if (num % j == 0) {
+        count++;
+      }
+    }
+    if(count==0){
+      sum+=num;
+    }
+    num+=2;
+  }
+  return sum;
 }
